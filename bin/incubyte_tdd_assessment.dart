537cc1f8d75080f17a06a6369bb598290943a301
cupcake08 <bhankhariaa@gmail.com>
2025-08-29 12:04:12 +0530

    feat: Initial project structure and dependencies

---
+++ bin/incubyte_tdd_assessment.dart
@@ -0,0 +1,5 @@
+import 'package:incubyte_tdd_assessment/incubyte_tdd_assessment.dart' as incubyte_tdd_assessment;
+
+void main(List<String> arguments) {
+  print('Hello world: ${incubyte_tdd_assessment.calculate()}!');
+}
---
+++ test/incubyte_tdd_assessment_test.dart
@@ -0,0 +1,8 @@
+import 'package:incubyte_tdd_assessment/incubyte_tdd_assessment.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
